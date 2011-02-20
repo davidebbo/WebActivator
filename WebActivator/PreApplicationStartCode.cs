bd4085255574f8e6d846db7e762221b3a61d9792
JakubKonecki <devnull@localhost>
2011-02-20 23:25:59 +0000

    Added support for executing WebActivator outside ASP.NET runtime.

--- WebActivator/PreApplicationStartCode.cs
+++ WebActivator/PreApplicationStartCode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Web;
@@ -6,34 +7,56 @@ using System.Web;
 namespace WebActivator {
     public class PreApplicationStartCode {
         static List<PreApplicationStartMethodAttribute> _attribsToCallAfterStart = new List<PreApplicationStartMethodAttribute>();
+		private static object initLock = new object();
+		private static bool hasInited;
 
         public static void Start() {
-            // Go through all the bin assemblies
-            foreach (var assemblyFile in Directory.GetFiles(HttpRuntime.BinDirectory, "*.dll")) {
-                var assembly = Assembly.LoadFrom(assemblyFile);
+			lock (initLock)
+			{
+				if (!hasInited)
+				{
+					// Go through all the bin assemblies
+					foreach (var assemblyFile in GetAssemblyFiles())
+					{
+						var assembly = Assembly.LoadFrom(assemblyFile);
 
-                // Go through all the PreApplicationStartMethodAttribute attributes
-                // Note that this is *our* attribute, not the System.Web namesake
-                foreach (PreApplicationStartMethodAttribute preStartAttrib in assembly.GetCustomAttributes(
-                    typeof(PreApplicationStartMethodAttribute),
-                    inherit: false)) {
+						// Go through all the PreApplicationStartMethodAttribute attributes
+						// Note that this is *our* attribute, not the System.Web namesake
+						foreach (PreApplicationStartMethodAttribute preStartAttrib in assembly.GetCustomAttributes(
+							typeof(PreApplicationStartMethodAttribute),
+							inherit: false))
+						{
 
-                    // If it asks to be called after global.asax App_Start, keep track of the method. Otherwise call it now
-                    if (preStartAttrib.CallAfterGlobalAppStart) {
-                        _attribsToCallAfterStart.Add(preStartAttrib);
-                    }
-                    else {
-                        // Invoke the method that the attribute points to
-                        preStartAttrib.InvokeMethod();
-                    }
-                }
-            }
+							// If it asks to be called after global.asax App_Start, keep track of the method. Otherwise call it now
+							if (preStartAttrib.CallAfterGlobalAppStart)
+							{
+								_attribsToCallAfterStart.Add(preStartAttrib);
+							}
+							else
+							{
+								// Invoke the method that the attribute points to
+								preStartAttrib.InvokeMethod();
+							}
+						}
+					}
 
-            // If any method needs to be called later, register our module to do it
-            if (_attribsToCallAfterStart.Count > 0) {
-                Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(StartMethodCallingModule));
-            }
+					// If any method needs to be called later, register our module to do it
+					if (_attribsToCallAfterStart.Count > 0)
+					{
+						Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(StartMethodCallingModule));
+					}
+
+					hasInited = true;
+				}
+			}
         }
+
+		private static IEnumerable<string> GetAssemblyFiles()
+		{
+			string directory = HttpContext.Current == null ? Environment.CurrentDirectory : HttpRuntime.BinDirectory;
+
+			return Directory.GetFiles(directory, "*.dll");
+		}
 
         class StartMethodCallingModule : IHttpModule {
             private static object initLock = new object();
