503a103490ce5db0994994d2f2f5e96d366f7ebe
davidebbo <david.ebbo@microsoft.com>
2011-02-28 18:22:41 -0800

    Bumped version to 1.4

--- WebActivator/Properties/AssemblyInfo.cs
+++ WebActivator/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Web;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
 
 [assembly: PreApplicationStartMethod(typeof(WebActivator.ActivationManager), "Run")]
